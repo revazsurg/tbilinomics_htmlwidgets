f46e105dbec3e0f03f049d470d908a7124bf82d3
Revaz <revazsurg@gmail.com>
2019-03-23 22:53:37 +0400

    Initial commit

---
+++ R/rnormHexbin.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+rnormHexbin <- function(stdev, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    stdev = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'rnormHexbin',
+    x,
+    width = width,
+    height = height,
+    package = 'tbilinomics.htmlwidgets',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for rnormHexbin
+#'
+#' Output and render functions for using rnormHexbin within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a rnormHexbin
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name rnormHexbin-shiny
+#'
+#' @export
+rnormHexbinOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'rnormHexbin', width, height, package = 'tbilinomics.htmlwidgets')
+}
+
+#' @rdname rnormHexbin-shiny
+#' @export
+renderRnormHexbin <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, rnormHexbinOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/rnormHexbin.js
@@ -0,0 +1,182 @@
+HTMLWidgets.widget({
+
+  name: 'rnormHexbin',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    const margin = {
+      top: 40,
+      bottom: 40,
+      left: 40,
+      right: 40
+    };
+    
+    const histogram = {
+      height: 100
+    }
+    
+    const w = height - margin.left - margin.right;
+    const h = height - margin.top - margin.bottom;
+
+    var svg = d3.select(el).append("svg")
+      .attr("width", height + histogram.height + margin.top)
+      .attr("height", height + histogram.height + margin.top);
+
+    return {
+
+      renderValue: function(widgetInput) {
+
+        // Generate data points
+        
+        const randX = d3.randomNormal(w / 2, widgetInput.stdev[0]);
+        const randY = d3.randomNormal(h / 2, widgetInput.stdev[1]);
+        
+        const points = d3.range(20000).map(() => [randX(), randY()]);
+        
+        // Plot hexbin
+        
+        const hexbin = d3.hexbin().size([w, h]).radius(5);
+        
+        const color = d3.scaleLinear().domain([0,10])
+          .range(["white", "steelblue"])
+          .interpolate(d3.interpolateRgb);
+        
+        const hexPlot = svg.append("g")
+          .attr("class", "hex-plot")
+          .attr("transform", "translate(" + margin.left + "," + (2 * margin.top + histogram.height) + ")");
+        
+        hexPlot.append("clipPath")
+          .attr("id", "hex-clip")
+          .append("rect")
+          .attr("width", w)
+          .attr("height", h)
+        
+        hexagon = hexPlot.append("g")
+          .attr("clip-path", "url(#hex-clip)")
+          .selectAll(".hexagon")
+          .data(hexbin(points));
+         
+        hexagon.enter()
+          .append("path")
+          .attr("class", "hexagon")
+          .attr("transform", d => "translate(" + d.x + "," + d.y + ")")
+          .attr("d", hexbin.hexagon())
+          .style("fill", d => color(d.length));
+        
+        // Add axes
+        
+        const xScale = d3.scaleIdentity().domain([0, w]);
+        const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
+        
+        const xAxis = d3.axisBottom(xScale);
+        const yAxis = d3.axisLeft(yScale);
+        const yAxisRight = d3.axisRight(yScale);
+        
+        hexPlot.append("g").attr("class", "hex-x-axis")
+          .attr("transform", "translate(0," + h + ")")
+          .call(xAxis);
+          
+        hexPlot.append("g").attr("class", "hex-x-axis axis-top")
+          .call(xAxis.tickSize(0).tickValues([]));
+          
+        hexPlot.append("g").attr("class", "hex-y-axis")
+          .call(yAxis);
+          
+        hexPlot.append("g").attr("class", "hex-y-axis axis-right")
+          .attr("transform", "translate(" + w + ", 0)")
+          .call(yAxisRight.tickSize(0).tickValues([]));
+        
+        // Add histograms
+        
+        const bins = d3.histogram()
+          .domain(xScale.domain())
+          .thresholds(xScale.ticks(70));
+        
+        // Add top histogram
+        
+        const histTop = bins(points.map(d => d[0]));
+        
+        const gHistTop = svg.append("g")
+          .attr("class", "hist-top")
+          .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")");
+          
+        gHistTop.append("g")
+          .attr("class", "hist-x-axis")
+          .attr("transform", "translate(0," + histogram.height + ")")
+          .call(xAxis.tickSize(6).tickValues([0, 50, 100, 150, 200, 250, 300]));
+          
+        const gHistTopChart = gHistTop.append("g")
+          .attr("class", "hist-chart");
+        
+        const yScaleTop = d3.scaleLinear()
+          .domain([0, d3.max(histTop, d => d.length)])
+          .range([0, histogram.height]);
+          
+        gHistTopChart.selectAll(".bar")
+          .data(histTop)
+          .enter()
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", d => d.x0)
+          .attr("y", d => (histogram.height - yScaleTop(d.length)))
+          .attr("width", d => xScale(d.x1 - d.x0) - 1)
+          .attr("height", d => yScaleTop(d.length))
+          .style("fill", "steelblue");
+        
+        // Add right histogram
+          
+        const histRight = bins(points.map(d => d[1]));
+        
+        const gHistRight = svg.append("g")
+          .attr("class", "hist-right")
+          .attr("transform", "translate(" + (margin.left + w + margin.right) + "," + (2 * margin.top) + ")");
+        
+        gHistRight.append("g")
+          .attr("class", "hist-y-axis")
+          .attr("transform", "translate(0" + "," + histogram.height + ")")
+          .call(yAxis.tickSize(6).tickValues([0, 50, 100, 150, 200, 250, 300]));
+          
+        const gHistRightChart = gHistRight.append("g")
+          .attr("class", "hist-chart")
+          .attr("transform", "translate(0," + histogram.height + ")");
+        
+        const yScaleRight = d3.scaleLinear()
+          .domain([0, d3.max(histRight, d => d.length)])
+          .range([0, histogram.height]);
+          
+        gHistRightChart.selectAll(".bar")
+          .data(histRight)
+          .enter()
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", 0)
+          .attr("y", d => d.x0)
+          .attr("width", d => yScaleRight(d.length))
+          .attr("height", d => xScale(d.x1 - d.x0) - 1)
+          .style("fill", "steelblue");
+        
+      },
+
+      resize: function(width, height) {
+
+        const margin = {
+          top: 40,
+          bottom: 40,
+          left: 40,
+          right: 40
+        };
+    
+        const histogram = {
+          height: 100
+        }
+
+        var svg = d3.select(el).append("svg")
+          .attr("width", height + histogram.height + margin.top)
+          .attr("height", height + histogram.height + margin.top);
+          
+      }
+    };
+  }
+});
